d0d2a64749e8577223acabc1bc2b3f77340fa214
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-19 20:55:29 +0200

    Create a read user binary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-extern crate diesel_sandbox;
+extern crate dotenv;
+extern crate diesel;
+extern crate diesel_sandbox as ds;
 
+use diesel::prelude::*;
+use dotenv::dotenv;
+use ds::database::establish_connection;
+use ds::model::User;
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    use ds::schema::users::dsl::*;
+
+    let connection = establish_connection();
+    let results = users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+    for user in results {
+        println!("{:?}", user);
+    }
 }
---
+++ src/model/mod.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, Queryable)]
+pub struct User {
+    id: i32,
+    username: String,
+}
