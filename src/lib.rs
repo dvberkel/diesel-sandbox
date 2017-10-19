246cf98b95f851e2e6c4242a274888d9d7b5f642
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-10-19 20:24:23 +0200

    Create a skeletal diesel project

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+
+pub mod schema;
+pub mod database;
+pub mod model;
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+extern crate diesel_sandbox;
+
+
+fn main() {
+    println!("Hello, world!");
+}
